53f52a20b2ef58b4fdc7bd2b1338c2310e7844ab
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-13 10:28:18 +0100

    changed page info

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,95 @@
 
 <template>
-  
+  <div class="info-page hide " id="info-page">
+    <div class="content">
+        <div>
+            <h1 id="info-title" class="info-title">Quiz PHP!</h1>
+            <p id="content" >The test contains 10 questions and there is no time limit.<br>
+                The test is not official, <br> 
+                it's just a nice way to see how much you know, or don't know, about PHP. </p>
+            <button id="start"  class="start-btn ">Start Quiz </button>
+        </div>
+        <div id="image">
+            <img class="img-info" src="./assets/PHP6.png" alt="image">
+        </div>
+    </div>
    
 
+</div>
 </template>
-<style >
 
+<style scoped>
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+template {
+  background-image: linear-gradient(to right, #430D7D 18%, #964185 100%);
+  -webkit-animation: transitionIn-Y-bottom 0.5s;
+          animation: transitionIn-Y-bottom 0.5s;
+}
+#info-page {
+  height: 100vh;
+  background: linear-gradient(to right, #430D7D 18%, #471a3e 100%);
+}
+.info-page {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  min-height: 600px;
+  -webkit-animation: transitionIn-Y-bottom 0.5s;
+          animation: transitionIn-Y-bottom 0.5s;
+}
+.info-page .content {
+  display: flex;
+  top: 100px;
+}
+.info-page .content p {
+  border-color: #E8A69A;
+  box-sizing: border-box;
+  color: #f8fafa;
+  margin-left: 58px;
+  font-size: 19px;
+  padding: 33px;
+}
+.info-page .content .info-title {
+  top: 300px;
+  left: 100px;
+  color: #C01EA0;
+  justify-content: center;
+  align-items: center;
+  margin-left: 100px;
+}
+.info-page .img-info {
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+  margin-left: 50px;
+}
+.start-btn, .res-btn {
+  border: none;
+  padding: 2px 15px;
+  width: 130px;
+  height: 60px;
+  margin-left: 80px;
+  font-size: 20px;
+  cursor: pointer;
+  color: aliceblue;
+  background: #C3119F;
+  border-radius: 8px;
+}
+
+.infoTitle {
+  top: 300px;
+  left: 100px;
+  color: #C01EA0;
+  justify-content: center;
+  align-items: center;
+  margin-left: 550px;
+}
 </style>
---
+++ src/components/result.vue
@@ -0,0 +1,58 @@
+<script>
+
+</script>
+
+<template>
+    <div class="status hide" id="status-score">
+        <div class="circular-progress hide">
+          <div class="value-container hide">0%</div>
+        </div>
+      </div> 
+</template>
+
+<style scoped>
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  background: linear-gradient(to right, #430D7D 18%, #471a3e 100%);
+}
+
+.status {
+  height: 400px;
+  width: 400px;
+  background-color: #ffffff;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  border-radius: 8px;
+  display: grid;
+  place-items: center;
+}
+
+.circular-progress {
+  position: relative;
+  height: 250px;
+  width: 250px;
+  background-color: rgb(207, 38, 156);
+  box-shadow: 20px 20px 40px rgba(60, 60, 150, 0.25);
+  border-radius: 50%;
+  display: grid;
+  place-items: center;
+}
+.circular-progress::before {
+  content: "";
+  position: absolute;
+  height: 84%;
+  width: 84%;
+  background-color: #ffffff;
+  border-radius: 50%;
+}
+</style>
