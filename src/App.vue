ddaa1d6355ec8addef44a9b36d9c402bcbdbb2a6
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-13 09:58:46 +0100

    add component stepper

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,9 @@
-<!-- <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  
+   
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
-<script>
-  // import {ref} from "vue";
-  // const count = ref(0);
-  // console.log(count);
-  export default ({
-    data(){
-      return{
-        count:0
-      }
-    },
-    methods:{
-      addCount(){
-        // console.log("hiiiiiii");
-        this.count = this.count +1
-      }, 
-      subtructCount(){
-        this.count = this.count - 1
-      }
-    }
-
-  })
-</script>
-<template>
-  <div>
-    <h4>
-      counter is :
-    </h4>
-    <h1>{{count}}</h1>
-    <button @click="subtructCount()">-</button>
-    <button @click="addCount()">+</button>
-  </div>
 </template>
 <style >
-div{
-  background-color: rgb(214, 186, 240);
-}
+
 </style>
---
+++ src/components/stepper.vue
@@ -0,0 +1,68 @@
+<script setup>
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: true
+//   }
+// })
+</script>
+
+<template>
+  <div class="container" id="stepper">
+     
+    <div class="progress-container"> 
+        <div class="progress" id="progress"></div>
+        <div class="circle active " id="circle">1</div>
+        <div class="circle   " id="circle2">2</div>
+        <div class="circle " id="circle3">3</div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+.progress-container {
+  display: flex;
+  margin: 0 auto 30px;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  width: 350px;
+  max-width: 100%;
+}
+.progress-container::before {
+  content: "";
+  width: 100%;
+  background-color: white;
+  height: 5px;
+  position: absolute;
+  z-index: -1;
+}
+.progress {
+  width: 0;
+  background-color: #c3119f;
+  height: 5px;
+  top: 50%;
+  position: absolute;
+  transform: translateY(-50%);
+  z-index: -1;
+  transition: 0.4s ease-in-out;
+}
+
+.circle {
+  background: #fff;
+  height: 30px;
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid #f1f1f1;
+  border-radius: 50px;
+}
+
+.active {
+  background-color: #c3119f;
+  color: #fff;
+  transition: 0.6s ease-in;
+}
+</style>
