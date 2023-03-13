4a9cc7822820018d4c8f7c34f2dc3d1cb968db00
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-13 10:09:47 +0100

    add component info page

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -1,86 +1,78 @@
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
+  <div class="info-page hide " id="info-page">
+    <div class="content">
+        <div>
+            <h1 id="info-title" class="info-title">Quiz PHP!</h1>
+            <p id="content" >The test contains 10 questions and there is no time limit.<br>
+                The test is not official, <br> 
+                it's just a nice way to see how much you know, or don't know, about PHP. </p>
+            <button id="start"  class="start-btn ">Start Quiz </button>
+        </div>
+        <div id="image">
+            <img class="img-info" src="./assets/PHP6.png" alt="image">
+        </div>
     </div>
-  </div>
+   
+
+</div>
 </template>
 
 <style scoped>
-.item {
-  margin-top: 2rem;
+
+.info-page {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  min-height: 600px;
+  -webkit-animation: transitionIn-Y-bottom 0.5s;
+          animation: transitionIn-Y-bottom 0.5s;
+}
+.info-page .content {
   display: flex;
+  top: 100px;
+}
+.info-page .content p {
+  border-color: #E8A69A;
+  box-sizing: border-box;
+  color: #f8fafa;
+  margin-left: 58px;
+  font-size: 19px;
+  padding: 33px;
+}
+.info-page .content .info-title {
+  top: 300px;
+  left: 100px;
+  color: #C01EA0;
+  justify-content: center;
+  align-items: center;
+  margin-left: 100px;
+}
+.info-page .img-info {
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+  margin-left: 50px;
+}
+.start-btn, .res-btn {
+  border: none;
+  padding: 2px 15px;
+  width: 130px;
+  height: 60px;
+  margin-left: 80px;
+  font-size: 20px;
+  cursor: pointer;
+  color: aliceblue;
+  background: #C3119F;
+  border-radius: 8px;
 }
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
+.infoTitle {
+  top: 300px;
+  left: 100px;
+  color: #C01EA0;
+  justify-content: center;
+  align-items: center;
+  margin-left: 550px;
 }
 </style>
